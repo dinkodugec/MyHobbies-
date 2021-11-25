26e18cbeb71c2ab572015da64b4bd1a35a1d6bd9
Dinko <dinko.dugec@gmail.com>
2021-11-25 23:17:44 +0100

    Hello Laravel From županja

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Laravel from Županja :)
                 </div>
 
                 <div class="links">
