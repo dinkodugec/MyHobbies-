a6cd7df4fed9da0b2fe229cd69ed82621c043de9
Dinko <dinko.dugec@gmail.com>
2021-12-06 23:36:42 +0100

    seeder, faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,10 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->name,  //$faker -  for faking names, email adress
         'email' => $faker->unique()->safeEmail,
+        'motto' => $faker->realText(30),
+        'about_me' => $faker->realText(),
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
