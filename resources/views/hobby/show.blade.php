2fbd1487247c614b68108a9303fe878a72d8c0d2
Dinko <dinko.dugec@gmail.com>
2021-12-10 13:25:50 +0100

    Tag statistics-how many hobbies under one tag

--- resources/views/hobby/show.blade.php
+++ resources/views/hobby/show.blade.php
@@ -30,9 +30,9 @@
                     </div>
                 </div>
 
-                <div class="mt-2">
+               <!--  <div class="mt-2">
                 <a class="btn btn-primary btn-sm" href="{{ URL::previous() }}"><i class="fas fa-arrow-circle-up"></i> Back to Overview</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ Route::get('/hobby/tag/{tag_id}', 'hobbyTagController@getFilteredHobbies')->name
 // Attach / Detach Tags to Hobbies  2.step 
 Route::get('/hobby/{hobby_id}/tag/{tag_id}/attach', 'hobbyTagController@attachTag');
 Route::get('/hobby/{hobby_id}/tag/{tag_id}/detach', 'hobbyTagController@detachTag');
+
+/* Filtered View */
+Route::get('/hobby/tag/{tag_id}', 'hobbyTagController@getFilteredHobbies')->name('hobby_tag');
