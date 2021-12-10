0abe5fc45eaf7de8f5b7112d0b338873ee865fc9
Dinko <dinko.dugec@gmail.com>
2021-12-10 22:31:36 +0100

    prepare frontend for images

--- resources/views/hobby/index.blade.php
+++ resources/views/hobby/index.blade.php
@@ -5,8 +5,8 @@
         <div class="row justify-content-center">
             <div class="col-md-11">
                 <div class="card">
-                 
-                @isset($filter)
+
+                    @isset($filter)
                         <div class="card-header">Filtered hobbies by
                             <span style="font-size: 130%;" class="badge badge-{{ $filter->style }}">{{ $filter->name }}</span>
                             <span class="float-right"><a href="/hobby">Show all Hobbies</a></span>
@@ -14,47 +14,48 @@
                     @else
                         <div class="card-header">All the hobbies</div>
                     @endisset
-                    
-                  
 
                     <div class="card-body">
                         <ul class="list-group">
                             @foreach($hobbies as $hobby)
                                 <li class="list-group-item">
-                                <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby->name }}</a>
-                                @auth 
-                                <a class="btn btn-sm btn-light ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit Hobby</a> 
-                                 @endauth
-                                <span class="mx-2">Posted by: <a href="/user/{{$hobby->user->user_id}}" {{ $hobby->user->name }}</a> ({{ $hobby->user->hobbies->count() }} Hobbies)</span>
-                                @auth 
-                                <form class="float-right" style="display: inline" action="/hobby/{{ $hobby->id }}" method="post">
+                                    <a title="Show Details" href="/hobby/{{ $hobby->id }}">
+                                        <img src="/img/thumb_landscape.jpg" alt="thumb">
+                                        {{ $hobby->name }}
+                                    </a>
+                                    @auth
+                                    <a class="btn btn-sm btn-light ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit Hobby</a>
+                                    @endauth
+                                    <span class="mx-2">Posted by: <a href="/user/{{ $hobby->user->id }}">{{ $hobby->user->name }} ({{ $hobby->user->hobbies->count() }} Hobbies)</a>
+                                    <a href="/user/{{ $hobby->user->id }}"><img class="rounded" src="/img/thumb_portrait.jpg"></a>
+                                    </span>
+                                    @auth
+                                    <form class="float-right" style="display: inline" action="/hobby/{{ $hobby->id }}" method="post">
                                         @csrf
                                         @method("DELETE")
                                         <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
                                     </form>
-                                @endauth
-
-                               <!--  <span>{{ $hobby->created_at->diffForHumans() }}</span> --> 
-                               <span class="float-right mx-2">{{ $hobby->created_at->diffForHumans() }}</span>
+                                    @endauth
+                                    <span class="float-right mx-2">{{ $hobby->created_at->diffForHumans() }}</span>
                                     <br>
                                     @foreach($hobby->tags as $tag)
-                                    <a href="/hobby/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
+                                        <a href="/hobby/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
                                     @endforeach
                                 </li>
                             @endforeach
                         </ul>
                     </div>
                 </div>
- 
+
                 <div class="mt-3">
                     {{ $hobbies->links() }}
                 </div>
-                @auth 
+                @auth
                 <div class="mt-2">
                     <a class="btn btn-success btn-sm" href="/hobby/create"><i class="fas fa-plus-circle"></i> Create new Hobby</a>
                 </div>
-                    @endauth
+                @endauth
             </div>
         </div>
     </div>
-@endsection 
+@endsection
